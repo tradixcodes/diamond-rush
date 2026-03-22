501d025383c525140b351a05623631c521c9177f
tradixcodes <tradixiv@gmail.com>
2026-03-22 12:52:38 +0300

    F: imported player sprite walking animation, movement mechanism is bogus

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ function love.load()
 	sti = require("library/Simple-Tiled-Implementation/sti")
 	-- we are basically using this library only for the camera capablities
 	cameraFile = require("library/hump/camera")
+	anim8 = require("library/anim8/anim8")
 
 	--creates a camera object
 	cam = cameraFile()
@@ -18,12 +19,19 @@ function love.load()
 	world:addCollisionClass("Player" --[[, { ignores = { "Platform" } }]])
 
 	sprites = {}
+	sprites.playerSheet = love.graphics.newImage("sprites/walking_animation.png")
 
+	local grid = anim8.newGrid(32, 33, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+
+	animations = {}
+	animations.walk = anim8.newAnimation(grid("1-3", 1), 0.05)
 	-- creates a body
 	playerStartX, playerStartY = 32, 192
 	player = world:newRectangleCollider(playerStartX, playerStartY, 30, 30)
 	player.isMoving = false
 	player.speed = 250
+	player.animation = animations.walk
+	player.direction = 1
 	player:setFixedRotation(true)
 	player:setCollisionClass("Player")
 
@@ -53,7 +61,9 @@ function love.draw()
 	cam:attach()
 	gameMap:drawLayer(gameMap.layers["background"])
 	gameMap:drawLayer(gameMap.layers["walls"])
-	world:draw()
+	-- world:draw()
+	local px, py = player:getPosition()
+	player.animation:draw(sprites.playerSheet, px, py, nil, 1 * player.direction, 1, 16, 16.5)
 	cam:detach()
 	love.graphics.printf(
 		"Player Hitbox: " .. math.floor(px) .. ", " .. math.floor(py),
